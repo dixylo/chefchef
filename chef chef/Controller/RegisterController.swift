1230d58d0036f6d92debb8c45c755f8902347c42
dixylo <bixylim@gmail.com>
2018-06-14 15:56:50 +1200

    Some tweaks on logo and code for registering

--- chef chef/Controller/RegisterController.swift
+++ chef chef/Controller/RegisterController.swift
@@ -13,8 +13,13 @@ class RegisterController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var usernameTextField: UITextField!
+    
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var ageTextField: UITextField!
     
     
     override func viewDidLoad() {
